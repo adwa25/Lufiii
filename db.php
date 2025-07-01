1a00f7fe6671faed402ea302708277fba8522372
adwa25 <Zzilongcc@gmail.com>
2025-07-01 14:32:19 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "codashop";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// query untuk mengambil data top up
+function get_top_up() {
+    global $conn;
+    $sql = "SELECT * FROM top_up";
+    $result = $conn->query($sql);
+    return $result;
+}
+
+// query untuk menambahkan data top up
+function add_top_up($nominal) {
+    global $conn;
+    $sql = "INSERT INTO top_up (nominal) VALUES ('$nominal')";
+    return $conn->query($sql) === TRUE;
+}
+
+// query untuk menghapus data top up
+function delete_top_up($id) {
+    global $conn;
+    $sql = "DELETE FROM top_up WHERE id = '$id'";
+    return $conn->query($sql) === TRUE;
+}
+?>
